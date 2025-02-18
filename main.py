1bbf7c050c46d6a45c7461afb527c0a539ad9dcf
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2025-02-18 02:15:08 -0800

    init opencv

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+#This is setting the size for the Video Capture
+capture = cv.VideoCapture(0, cv.CAP_DSHOW)
+capture.set(cv.CAP_PROP_FRAME_WIDTH, 600)
+capture.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
+
+while True:
+    success, frame = capture.read()
+
+    if success:
+        cv.imshow("Captured Image", frame)
+        if cv.waitKey(1) != -1:
+            break
+
+cv.destroyAllWindows()  
